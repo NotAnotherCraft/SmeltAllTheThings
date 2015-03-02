eb65f2c8df1db494be980dd2e5602423bb1b61bf
KJ4IPS <aaron@haun.guru>
2015-03-02 11:26:55 -0500

    Initial terrasteel, and moving the concept of metal registration to SATTHook

--- src/main/java/net/notanothercraft/satt/hooks/SATTBotaniaHook.java
+++ src/main/java/net/notanothercraft/satt/hooks/SATTBotaniaHook.java
@@ -3,16 +3,23 @@ package net.notanothercraft.satt.hooks;
 import net.notanothercraft.satt.SATTMod;
 import net.notanothercraft.satt.metals.SATTMetal;
 import net.notanothercraft.satt.metals.botania.MetalManaSteel;
+import net.notanothercraft.satt.metals.botania.MetalTerraSteel;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Created by KJ4IPS on 3/1/2015.
  */
 public class SATTBotaniaHook extends SATTHook{
 
+
     @Override
     public void Engage() {
-        SATTMod.getLogger().info("Registering Manasteel");
-        SATTMetal manasteel = new MetalManaSteel();
-        manasteel.registerAll(null);
+        addMetal(MetalManaSteel.class);
+        addMetal(MetalTerraSteel.class);
+        registerMetals();
     }
 }
--- src/main/java/net/notanothercraft/satt/hooks/SATTHook.java
+++ src/main/java/net/notanothercraft/satt/hooks/SATTHook.java
@@ -1,8 +1,32 @@
 package net.notanothercraft.satt.hooks;
 
+import net.notanothercraft.satt.SATTMod;
+import net.notanothercraft.satt.metals.SATTMetal;
+
+import java.util.ArrayList;
+
 /**
  * Created by KJ4IPS on 3/1/2015.
  */
 public abstract class SATTHook {
     public abstract void Engage();
+
+
+    private ArrayList<Class<? extends SATTMetal>> metals = new ArrayList<Class<? extends SATTMetal>>();
+
+    public void addMetal(Class<? extends SATTMetal> clazz) {
+        metals.add(clazz);
+    }
+
+    public void registerMetals() {
+        for (Class<? extends SATTMetal> clazz : metals) {
+            SATTMod.getLogger().info("Registering " + clazz.getSimpleName());
+            try {
+                clazz.getConstructor().newInstance().registerAll();
+            } catch (Exception e) {
+                SATTMod.getLogger().error("Could not register " + clazz.getSimpleName());
+                e.printStackTrace();
+            }
+        }
+    }
 }
